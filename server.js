2e280530439ddf88490aa91161f4b11bdd8f0485
Camilo Garma <ctgarma@gmail.com>
2018-02-04 18:55:29 +1100

    added explicit json return in express route

--- server.js
+++ server.js
@@ -12,23 +12,26 @@ app.use(bodyParser.urlencoded({
 app.get('/', (req, res) => {
   m.main().then(
     (data) => {
-      res.status(200).send(data);
+      res.status(200).json(data);
     }
   );
 });
 
+//unnecessary can be removed
 //test api  only since subtext api is not reliable
 app.get('/subTexts', (req, res) => {
   var text = {
     "subTexts": ["Peter", "peter", "Pick", "Pi", "Z"]
   };
-  res.send(JSON.stringify(text), null, 4);
+  res.status(200).json(text);
 });
 
+//unnecessary can be removed
 // test api for postresult api since api provided is not reliable
 app.post('/postresults', (req, res) => {
   res.status(200).send(req.body);
 });
+
 
 app.listen(port, () => {
   console.log('started at port ', 3000);
